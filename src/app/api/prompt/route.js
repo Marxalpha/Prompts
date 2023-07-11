416231407423020d94f8dd75abfd5f74c34b7994
Marxalpha <saikrishnaknm@gmail.com>
2023-07-11 18:30:10 +0530

    Removed Sort
    used reverse instead of sort

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -4,11 +4,11 @@ import Prompt from "../../../../models/prompt";
 export async function GET(request){
     try{
         await connectToDB();
-        const prompts=await Prompt.find({}).populate('creator').sort({date:-1})
+        const prompts=await Prompt.find({}).populate('creator')
 
-        console.log({prompts})
+        // console.log(prompts.reverse())
         console.log("im in route of prompt api")
-        return new Response(JSON.stringify(prompts),{status:200})
+        return new Response(JSON.stringify(prompts.reverse()),{status:200})
     }
     catch(error){
         return new Response("Failed to fetch the cards",{status:500})
